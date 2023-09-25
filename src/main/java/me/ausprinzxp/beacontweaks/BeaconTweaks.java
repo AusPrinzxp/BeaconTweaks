feefc4ac36c9949257cca8c6cbc966af54039889
ausprinzxp <eugster.silvan@gmail.com>
2023-09-25 13:18:28 +0200

    init

---
+++ src/main/java/me/ausprinzxp/beacontweaks/BeaconTweaks.java
@@ -0,0 +1,17 @@
+package me.ausprinzxp.beacontweaks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BeaconTweaks extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
